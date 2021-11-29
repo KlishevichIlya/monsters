9b062b1212785d138c2d828f7894134c3ad8fb8e
Ilya Klishevich <klishevich.ilya@gmail.com>
2021-11-29 12:33:47 +0300

    Created searchField input. Monsters are filtered.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import {Component} from 'react';
 import {CardList} from "./components/card-list/card-list.component";
 import './App.css';
+import {SearchBox} from "./components/search-box/search-box.component";
 
 
 class App extends Component{
     constructor() {
         super();
         this.state = {
-            monsters: []
-        }
+            monsters: [],
+            searchField : '',
+        };
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
@@ -17,11 +20,23 @@ class App extends Component{
             .then(users => this.setState({monsters: users}))
     }
 
+    handleChange = (e) => {
+        this.setState({searchField: e.target.value});
+    }
 
     render() {
+        const {monsters, searchField} = this.state;
+        const filteredMonsters = monsters.filter(monster =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase())
+        )
+
         return (
             <div className="App">
-                <CardList monsters={this.state.monsters} />
+                <SearchBox
+                    placeholder='search monsters'
+                    handleChange={this.handleChange}
+                />
+                <CardList monsters={filteredMonsters} />
             </div>
         );
     }
